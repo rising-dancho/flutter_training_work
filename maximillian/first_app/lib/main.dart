97143535a350f9f5b8aef99143d8c434514350e2
Jose A. Perez Jr <EI0009E@EMPLEYADO.COM>
2026-03-13 15:38:01 +0800

    feat: training

---
+++ maximillian/first_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 52, 2, 131),
+                Color.fromARGB(255, 59, 7, 103),
+              ],
+              begin: AlignmentGeometry.topLeft,
+              end: AlignmentGeometry.bottomRight,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              "Hello World",
+              style: TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
+
+// Container is useful for styling
