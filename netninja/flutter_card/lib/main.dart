1283fcbdef16b7271f0ab936f3ae3cb56998d6d3
Jose A. Perez Jr <EI0009E@EMPLEYADO.COM>
2026-03-16 18:37:44 +0800

    feat: training

---
+++ netninja/flutter_card/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("YUKI YOZA"),
+          backgroundColor: Colors.brown[700],
+          centerTitle: true,
+        ),
+        body: const Home(),
+      ),
+    ),
+  );
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("betlogs!!!!!!!!!");
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
