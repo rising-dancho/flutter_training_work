6befbc57134f2e984e56b5804f5f06b63dedc7eb
Jose A. Perez Jr <EI0009E@EMPLEYADO.COM>
2026-03-30 12:07:51 +0800

    feat: training

--- figma_work/empleyado/lib/theme.dart
+++ figma_work/empleyado/lib/theme.dart
@@ -7,9 +7,15 @@ class AppColors {
   static Color redColor = Color.fromRGBO(189, 33, 48, 0);
   static Color redDarker = Color.fromRGBO(189, 33, 48, 0);
   static Color blueColor = Color.fromRGBO(93, 156, 201, 0);
-  static Color orangeColor = Color.fromRGBO(248, 187, 134, 0);
-  static Color greenColor = Color.fromRGBO(165, 220, 134, 0);
+  static Color blueLighter = Color.fromRGBO(235, 245, 251, 0);
+  static Color orangeColor = Color.fromRGBO(248, 156, 58, 0);
+  static Color yellowColor = Color.fromRGBO(212, 172, 13, 0);
+  static Color greyColor = Color.fromRGBO(229, 229, 229, 0);
+  static Color violetColor = Color.fromRGBO(129, 102, 122, 0);
+  static Color greenColor = Color.fromRGBO(82, 190, 128, 0);
 
-  static Color titleColor = Color.fromRGBO(0, 0, 0, 0);
-  static Color textColor = Color.fromRGBO(44, 44, 44, 0);
+  static Color titleColor = Color.fromRGBO(44, 44, 44, 0);
+  static Color textColor = Color.fromRGBO(44, 73, 70, 0);
+  static Color successColor = Color.fromRGBO(165, 220, 134, 0);
+  static Color informationColor = Color.fromRGBO(248, 187, 134, 0);
 }
